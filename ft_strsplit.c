4ce28b6dc79f809a748c2ab2ea2dd4ccb25081e5
Valeriia Muradian <vmuradia@e1z3r1p21.42.us.org>
2018-10-27 15:41:38 -0700

    haha

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,44 +6,69 @@
 /*   By: vmuradia <vmuradia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/24 21:01:33 by vmuradia          #+#    #+#             */
-/*   Updated: 2018/10/27 14:58:34 by vmuradia         ###   ########.fr       */
+/*   Updated: 2018/10/27 15:36:56 by vmuradia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char			**ft_strsplit(char const *s, char c)
+static int	ft_cnt_parts(const char *s, char c)
 {
-	size_t		i;
-	size_t		j;
-	size_t		words;
-	char		**res;
+	int		cnt;
+	int		in_substr;
+
+	in_substr = 0;
+	cnt = 0;
+	while (*s != '\0')
+	{
+		if (in_substr == 1 && *s == c)
+			in_substr = 0;
+		if (in_substr == 0 && *s != c)
+		{
+			in_substr = 1;
+			cnt++;
+		}
+		s++;
+	}
+	return (cnt);
+}
+
+static int	ft_wlen(const char *s, char c)
+{
+	int		len;
+
+	len = 0;
+	while (*s != c && *s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+char		**ft_strsplit(char const *s, char c)
+{
+	char	**t;
+	int		nb_word;
+	int		i;
 
 	i = 0;
 	if (!s)
 		return (NULL);
-	words = ft_cntwrd(s, c);
-	if (!(res = (char **)malloc(sizeof(char *) * (words + 1))))
+	nb_word = ft_cnt_parts((const char *)s, c);
+	t = (char **)malloc(sizeof(*t) * (ft_cnt_parts((const char *)s, c) + 1));
+	if (t == NULL)
 		return (NULL);
-	while (i < words)
+	while (nb_word--)
 	{
-		while (*s == c && *s)
+		while (*s == c && *s != '\0')
 			s++;
-		res[i] = (char *)malloc(sizeof(char) * (ft_strdellen(s, c) + 1));
-		j = 0;
-		while (*s != c && *s)
-			res[i][j++] = *s++;
-		res[i][j] = '\0';
+		t[i] = ft_strsub((const char *)s, 0, ft_wlen((const char *)s, c));
+		if (t[i] == NULL)
+			return (NULL);
+		s = s + ft_wlen(s, c);
 		i++;
 	}
-	res[i] = NULL;
-	return (res);
-}
-
-#include <stdio.h>
-
-int main()
-{
-	printf("%s", *ft_strsplit("hello", 'a'));
-
+	t[i] = NULL;
+	return (t);
 }
